7ef1bac753c73ec4b81b6a9739021afdf7193a73
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-27 16:05:07 +0530

    updated home page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -10,21 +11,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          HOLIDAY CHALLENGE
-        </h1>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            HOLIDAY CHALLENGE
+          </h1>
+          <p>
+          A week long challenge during the Holiday week. Your task is to create a holiday themed website like countdown to christmas site, holiday photos posting site, etc. using web technologies. An opportunity to test your wide variety of coding, design, and problem-solving skills The challenge covers a wide range of technologies, including HTML, CSS, JavaScript, React and more!
+          </p>
+          <div className={styles.btncontainer}>
+            <button id={styles.btn} className={styles.explore}>Explore</button>
+            <button id={styles.btn} className={styles.register}>Register</button>
+          </div>
+        </div>
+        <img src="/snowman.svg" alt="snowman" className={styles.snowman} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
