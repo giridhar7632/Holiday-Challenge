680be13d7ca8da71825f43fdbab169f6e2d020d3
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-28 17:44:20 +0530

    added components

---
+++ pages/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return(
+    <main>
+        <div>
+          <h1>
+            HOLIDAY CHALLENGE
+          </h1>
+        </div>
+        <img src="/snowman.svg" alt="snowman" />
+    </main>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from './components/Header'
 
 export default function Home() {
   
@@ -9,28 +10,7 @@ export default function Home() {
         <title>Holiday Overlay</title>
         <link rel="icon" href="/snowman.svg" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.section}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>
-              HOLIDAY CHALLENGE
-            </h1>
-            <p>
-            A week long challenge during the Holiday week. Your task is to create a holiday themed website like countdown to christmas site, holiday photos posting site, etc. using web technologies. An opportunity to test your wide variety of coding, design, and problem-solving skills The challenge covers a wide range of technologies, including HTML, CSS, JavaScript, React and more!
-            </p>
-            <div className={styles.btncontainer}>
-              <button id={styles.btn} className={styles.explore}>Explore</button>
-              <button id={styles.btn} className={styles.register}>Register</button>
-            </div>
-          </div>
-          <img src="/snowman.svg" alt="snowman" className={styles.snowman} />
-          <span className={styles.skewed}>
-          </span>
-        </div>
-        <div className={styles.section}>
-        </div>
-        <div className={styles.section}></div>
-      </main>
+      <Header />
     </div>
   )
 }
