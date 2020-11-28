be3697dcbce644aae2aecfe88742940cc3f49fee
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-28 08:13:33 +0530

    added shadow

--- pages/index.js
+++ pages/index.js
@@ -11,19 +11,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>
-            HOLIDAY CHALLENGE
-          </h1>
-          <p>
-          A week long challenge during the Holiday week. Your task is to create a holiday themed website like countdown to christmas site, holiday photos posting site, etc. using web technologies. An opportunity to test your wide variety of coding, design, and problem-solving skills The challenge covers a wide range of technologies, including HTML, CSS, JavaScript, React and more!
-          </p>
-          <div className={styles.btncontainer}>
-            <button id={styles.btn} className={styles.explore}>Explore</button>
-            <button id={styles.btn} className={styles.register}>Register</button>
+        <div className={styles.section}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>
+              HOLIDAY CHALLENGE
+            </h1>
+            <p>
+            A week long challenge during the Holiday week. Your task is to create a holiday themed website like countdown to christmas site, holiday photos posting site, etc. using web technologies. An opportunity to test your wide variety of coding, design, and problem-solving skills The challenge covers a wide range of technologies, including HTML, CSS, JavaScript, React and more!
+            </p>
+            <div className={styles.btncontainer}>
+              <button id={styles.btn} className={styles.explore}>Explore</button>
+              <button id={styles.btn} className={styles.register}>Register</button>
+            </div>
           </div>
+          <img src="/snowman.svg" alt="snowman" className={styles.snowman} />
+          <span className={styles.skewed} onScroll={() => {
+            var value = 10 + window.scrollY/60
+            this.style.transform = `skewY(${value}deg)`
+            console.log(value)
+          }}></span>
         </div>
-        <img src="/snowman.svg" alt="snowman" className={styles.snowman} />
       </main>
     </div>
   )
