94eebf2b814beb8c05d097053ae2593e043a55b5
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-30 09:10:46 +0530

    updated section

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,4 +1,4 @@
-import styles from '../../styles/Header.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Header() {
   const header={
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from './components/Header'
+import Mission from './components/Mission'
 
 export default function Home() {
   
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/snowman.svg" />
       </Head>
       <Header />
+      <Mission />
     </div>
   )
 }
