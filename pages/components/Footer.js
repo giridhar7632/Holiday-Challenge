5c4f795f3e420f1a73452f6037af01274b277318
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-30 16:46:24 +0530

    some cool changes

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -24,7 +24,6 @@ export default function Footer(){
         <button className={styles.btn}><a href="https://airtable.com/shrWZ1BTvA1GzzVnv"> Register Now! </a></button>
         <p className={styles.text}>Feel free to contact if you have any queries</p>
         <p className={styles.text}>&copy; Students Who <span className={styles.hilight}>Code</span> - 2020</p>
-
       </div>
     </div>
   )
