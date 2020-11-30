099f1ab1f1a7ec93d7b441276f8e13036cd30dbf
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-30 08:07:31 +0530

    Second day

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,12 +1,22 @@
+import styles from '../../styles/Header.module.css'
+
 export default function Header() {
+  const header={
+    height: "100vh",
+    background: "#206391",
+    color: "#E5E5EE"
+  }
   return(
-    <main>
-        <div>
-          <h1>
-            HOLIDAY CHALLENGE
-          </h1>
-        </div>
-        <img src="/snowman.svg" alt="snowman" />
-    </main>
+    <header style={header}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          HOLIDAY CHALLENGE
+        </h1>
+        <p className={styles.date}>19<sup>th</sup> - 25<sup>th</sup> December, <span className={styles.year}>2020</span></p>
+        <button className={styles.btn}> Register Now! </button>
+      </div>  
+      <img src="/snowman.svg" alt="snowman" className={styles.snowman} />
+      
+    </header>
   )
 }
